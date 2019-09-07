f0298362fee26b7923ab4f9d825cbac48354df79
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-09-07 18:28:39 -0300

    Add sign in skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Router } from '@reach/router'
 import BoardPage from './BoardPage'
+import SignInPage from './SignInPage'
 
 const App: React.FC = () => {
   return (
     <Router>
       <BoardPage path="/board"></BoardPage>
+      <SignInPage path="/login"></SignInPage>
     </Router>
   )
 }
---
+++ src/SignInPage.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { RouteComponentProps } from '@reach/router'
+import useForm from 'react-hook-form'
+
+const SignInPage: React.FC<RouteComponentProps> = () => {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = (data: any) => console.log(data)
+
+  return (
+    <div className="container">
+      <div className="row center-md">
+        <div className="col-md-4">
+          <div className="auth">
+            <form className="form form--dark" onSubmit={handleSubmit(onSubmit)}>
+              <div className="form__group">
+                <label htmlFor="">E-mail</label>
+                <input
+                  name="email"
+                  type="email"
+                  className="form__field"
+                  ref={register}
+                />
+              </div>
+
+              <div className="form__group">
+                <label htmlFor="">Senha</label>
+                <input
+                  name="password"
+                  type="password"
+                  className="form__field"
+                  ref={register}
+                />
+              </div>
+
+              <div className="form__group">
+                <button className="btn btn--success btn--block">Entrar</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SignInPage
