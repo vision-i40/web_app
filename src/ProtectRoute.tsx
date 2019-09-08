b2b830493bc2ab4646afaae27864575fa4b86cac
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-09-08 00:50:37 -0300

    Add ProtectRoute component

---
+++ src/ProtectRoute.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import AuthService from './AuthService'
+import { RouteComponentProps } from '@reach/router'
+
+type ProtectRouteState = {
+  isSafe: boolean
+}
+
+type ProtectRouteProps = {
+  redirectTo: string
+}
+
+const ProtectRoute: React.FC<ProtectRouteProps & RouteComponentProps> = ({
+  children,
+  redirectTo,
+  navigate
+}) => {
+  const [state, setState] = useState<ProtectRouteState>({ isSafe: false })
+
+  useEffect(() => {
+    AuthService.isAuthenticated()
+      ? setState({ isSafe: true })
+      : navigate && navigate(redirectTo)
+  }, [navigate, redirectTo])
+
+  return <>{state.isSafe && children}</>
+}
+
+export default ProtectRoute
