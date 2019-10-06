b4706e0efb52b25c7ffc4cc8ea7addb59c8728c4
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-10-06 01:55:03 -0300

    Update board view

--- src/views/ProductionLineBoard.tsx
+++ src/views/ProductionLineBoard.tsx
@@ -110,11 +110,19 @@ const ProductionLineBoard: React.FC<ProductionLineBoardProps> = ({
               </div>
               <div className="operation__main__info">
                 <hgroup>
-                  <h3 className="text-secondary">#{productionOrder.code}</h3>
-                  <h1>{productionOrder.product.name}</h1>
-                  <h3 className="text-secondary text-regular">
-                    Iniciado de 10:00:00
-                  </h3>
+                  {/* <h3 className="text-secondary">#{productionOrder.code}</h3> */}
+                  <h1 className="operation__main__info__title">
+                    {productionOrder.product.name}
+                  </h1>
+                  <div className="operation__main__info__secondary">
+                    <span className="operation__main__info__status">
+                      <i className="fas fa-check"></i> Ativo
+                    </span>
+
+                    <span className="operation__main__info__time">
+                      <i className="far fa-clock"></i> 10:00:00
+                    </span>
+                  </div>
                 </hgroup>
               </div>
             </div>
@@ -124,52 +132,122 @@ const ProductionLineBoard: React.FC<ProductionLineBoardProps> = ({
         <div className="operation__data">
           <div className="container">
             <div className="row">
-              <div className="col-xs-4">
-                <div className="operation__info">
-                  <div className="operation__info__label">Boas</div>
-                  <div className="operation__info__value">{productionOrder.production_quantity}</div>
+              <div className="col-xs-12 col-sm-4">
+                <div className="operation__card">
+                  <div className="operation__title">
+                    {/* <span className="operation__title__icon">
+                      <i className="fa fa-check"></i>
+                    </span>{' '} */}
+                    Produzidas
+                  </div>
+                  <div className="operation__value">
+                    {productionOrder.production_quantity}
+                  </div>
+
+                  <span className="operation__bg__icon">
+                    <i className="fa fa-check"></i>
+                  </span>
                 </div>
               </div>
 
-              <div className="col-xs-4">
-                <div className="operation__info operation__info--warning">
-                  <div className="operation__info__label">Rejeitadas</div>
-                  <div className="operation__info__value">{productionOrder.rework_quantity}</div>
+              <div className="col-xs-12 col-sm-4">
+                <div className="operation__card">
+                  <div className="operation__title">
+                    {/* <span className="operation__title__icon">
+                      <i className="fa fa-check"></i>
+                    </span>{' '} */}
+                    Retrabalho
+                  </div>
+                  <div className="operation__value">
+                    {productionOrder.rework_quantity}
+                  </div>
+
+                  <span className="operation__bg__icon operation__bg__icon--warning">
+                    <i className="fa fa-recycle"></i>
+                  </span>
                 </div>
               </div>
 
-              <div className="col-xs-4">
-                <div className="operation__info operation__info--danger-dark">
-                  <div className="operation__info__label">Refugos</div>
-                  <div className="operation__info__value">{productionOrder.waste_quantity}</div>
+              <div className="col-xs-12 col-sm-4">
+                <div className="operation__card">
+                  <div className="operation__title">
+                    {/* <span className="operation__title__icon">
+                      <i className="fa fa-check"></i>
+                    </span>{' '} */}
+                    Disperdiçadas
+                  </div>
+                  <div className="operation__value">
+                    {productionOrder.waste_quantity}
+                  </div>
+
+                  <span className="operation__bg__icon operation__bg__icon--danger">
+                    <i className="fas fa-trash-alt"></i>
+                  </span>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
+        {/* <div className="operation__data">
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-4">
+                <div className="operation__info">
+                  <div className="operation__info__label">Boas</div>
+                  <div className="operation__info__value">
+                    {productionOrder.production_quantity}
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-xs-4">
+                <div className="operation__info operation__info--warning">
+                  <div className="operation__info__label">Rejeitadas</div>
+                  <div className="operation__info__value">
+                    {productionOrder.rework_quantity}
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-xs-4">
+                <div className="operation__info operation__info--danger-dark">
+                  <div className="operation__info__label">Refugos</div>
+                  <div className="operation__info__value">
+                    {productionOrder.waste_quantity}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> 
+        </div> */}
+
         <div className="operation__actions">
           <div className="container">
             <button onClick={toggleNewGoodPieces} className="operation__btn">
               <i className="fas fa-plus"></i>
+              <p className="operation__btn__label">Produzida</p>
             </button>
             <button
               onClick={toggleNewRejectedPieces}
               className="operation__btn operation__btn--warning"
             >
               <i className="fas fa-recycle"></i>
-            </button>
-            <button
-              onClick={toggleNewStops}
-              className="operation__btn operation__btn--danger"
-            >
-              <i className="fas fa-ban"></i>
+              <p className="operation__btn__label">Retrabalho</p>
             </button>
             <button
               onClick={toggleNewScraps}
+              className="operation__btn operation__btn--danger"
+            >
+              <i className="fas fa-trash-alt"></i>
+              <p className="operation__btn__label">Disperdiçadas</p>
+            </button>
+            <button
+              onClick={toggleNewStops}
               className="operation__btn operation__btn--danger-dark"
             >
-              <i className="fas fa-recycle"></i>
+              <i className="fas fa-ban"></i>
+              <p className="operation__btn__label">Paradas</p>
             </button>
           </div>
         </div>
