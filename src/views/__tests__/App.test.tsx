c6d006478dd501389f31a66027c8a11bf1a65be3
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-10-12 10:56:33 -0300

    Move tests to __tests__ to avoid visual noise

--- src/views/__tests__/App.test.tsx
+++ src/views/__tests__/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import { AuthProvider } from './AuthProvider'
+import App from '../App'
+import { AuthProvider } from '../AuthProvider'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
